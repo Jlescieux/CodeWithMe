91ef8bfcf7b10e75a80c504c42a1994192d1f210
Jlescieux <julien.lescieux@outlook.com>
2020-04-17 16:08:16 +0200

    Fixtures finished

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,11 +3,16 @@
 namespace App\DataFixtures;
 
 use Faker\Factory;
+use App\Entity\Tag;
 use App\Entity\Role;
 use App\Entity\User;
+use App\Entity\Skill;
+use App\Entity\Follow;
 use App\Entity\Statut;
+use App\Entity\Techno;
 use App\Entity\Comment;
 use App\Entity\Project;
+use App\Entity\Collaboration;
 use App\DataFixtures\Provider;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -16,6 +21,7 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 class AppFixtures extends Fixture
 {
 
+    // Permet d'encoder les mots de passe en BDD
     private $encoder;
 
     public function __construct(UserPasswordEncoderInterface $encoder)
@@ -49,10 +55,10 @@ class AppFixtures extends Fixture
         // table "Role"
         $utilisateur = new Role();
         $utilisateur->setName('Utilisateur');
-        $manager->persist($utilisateur);   
+        $manager->persist($utilisateur);
         $Moderateur = new Role();
         $Moderateur->setName('Modérateur');
-        $manager->persist($Moderateur);        
+        $manager->persist($Moderateur);
         $administrateur = new Role();
         $administrateur->setName('Administrateur');
         $manager->persist($administrateur);
@@ -74,8 +80,8 @@ class AppFixtures extends Fixture
             ->setUrlLinkedin('https://fr.linkedin.com/in/jlescieux')
             ->setUrlGithub('https://github.com/Jlescieux')
             ->setRole($administrateur);
-        $manager->persist($admin);    
-        
+        $manager->persist($admin);
+
         // table "User"
         $users = [];
         for ($j = 0; $j < 20; $j++) {
@@ -120,13 +126,37 @@ class AppFixtures extends Fixture
                 ->setUrlGithub('https://github.com/' . $generator->userName() . '/' . $projects[$j]->getTitle())
                 ->setUrlTwitter('https://twitter.com/' . $projects[$j]->getTitle())
                 ->setUrlTipeee('https://fr.tipeee.com/' . $projects[$j]->getTitle());
-            // on récupère un statut et un créateur au hasard
-            $randomStatut = $status[rand(0, (count($status) - 1))];
-            $randomUser = $users[rand(0, (count($users) - 1))];
-            // puis on les ajoutes au projet
+            // nous récupèrons un statut et un créateur au hasard
+            $randomStatut = $status[mt_rand(0, (count($status) - 1))];
+            $randomUser = $users[mt_rand(0, (count($users) - 1))];
+            // puis nous les ajoutons au projet
             $projects[$j]->setStatut($randomStatut);
             $projects[$j]->setOwner($randomUser);
             $manager->persist($projects[$j]);
+        }
+
+        // table "Tag"
+        $tags = [];
+        for ($i = 0; $i < 10; $i++) {
+            $tags[$i] = new Tag();
+            $tags[$i]->setName($generator->unique()->TagName());
+            $manager->persist($tags[$i]);
+        }
+
+        // table "Techno"
+        $technos = [];
+        for ($i = 0; $i < 16; $i++) {
+            $technos[$i] = new Techno();
+            $technos[$i]->setName($generator->unique()->technoName());
+            $manager->persist($technos[$i]);
+        }
+
+        // table "Skill"
+        $skills = [];
+        for ($i = 0; $i < 10; $i++) {
+            $skills[$i] = new Skill();
+            $skills[$i]->setName($generator->unique()->skillName());
+            $manager->persist($skills[$i]);
         }
 
         // table "Comment"
@@ -135,13 +165,91 @@ class AppFixtures extends Fixture
             $comments[$k] = new Comment();
             $comments[$k]->setContent($generator->realText($maxNbChars = 100, $indexSize = 2))
                 ->setCreatedAt($generator->datetime('now', 'Europe/Paris'));
-            // on récupère un projet et un utilisateur au hasard
-            $randomProject = $projects[rand(0, (count($projects) - 1))];
-            $randomUser = $users[rand(0, (count($users) - 1))];
-            // puis on les ajoutes au commentaire
+            // nous récupèrons un projet et un utilisateur au hasard
+            $randomProject = $projects[mt_rand(0, (count($projects) - 1))];
+            $randomUser = $users[mt_rand(0, (count($users) - 1))];
+            // puis nous les ajoutons au commentaire
             $comments[$k]->setProject($randomProject);
             $comments[$k]->setUser($randomUser);
             $manager->persist($comments[$k]);
+        }
+
+        // table "Follow"
+        foreach ($projects as $project) {
+            shuffle($users);
+            $userCount = mt_rand(0, (count($users) - 1));
+            for ($i = 0; $i <= $userCount; $i++) {
+                $follow = new Follow;
+                $follow->setIsFollowed(true);
+                $follow->setProject($project);
+                $follow->setUser($users[$i]);
+                $manager->persist($follow);
+            }
+        }
+        
+        // REMPLIT LES TABLES DE RELATIONS "MANY TO MANY"
+
+        // table "project_tag"
+        foreach ($projects as $project) {
+            shuffle($tags);
+            $tagCount = mt_rand(1, 3);
+            for ($i = 1; $i <= $tagCount; $i++) {
+                $project->addTag($tags[$i]);
+            }
+            $manager->persist($project);
+        }
+
+        // table "project_techno"
+        foreach ($projects as $project){
+            shuffle($technos);
+            $technoCount = mt_rand(1, 5);
+            for ($i = 1; $i <= $technoCount; $i++) {
+                $project->addTechno($technos[$i]);
+            }
+            $manager->persist($project);
+        }
+
+        // table "project_skill"
+        foreach ($projects as $project) {
+            shuffle($skills);
+            $skillCount = mt_rand(1, 5);
+            for ($i = 1; $i <= $skillCount; $i++) {
+                $project->addSkill($skills[$i]);
+            }
+            $manager->persist($project);
+        }
+
+         // table "user_skill"
+         foreach ($users as $user) {
+             shuffle($skills);
+             $skillCount = mt_rand(1, 2);
+             for ($i = 1; $i <= $skillCount; $i++) {
+                 $user->addSkill($skills[$i]);
+             }
+             $manager->persist($user);
+         }
+ 
+         // table "user_techno"
+         foreach ($users as $user) {
+             shuffle($technos);
+             $technoCount = mt_rand(1, 5);
+             for ($i = 1; $i <= $technoCount; $i++) {
+                 $user->addTechno($technos[$i]);
+             }
+             $manager->persist($user);
+         }
+        
+        // table "Collaboration"
+        foreach ($projects as $project) {
+            shuffle($users);
+            $userCount = mt_rand(0, 5);
+            for ($i = 0; $i <= $userCount; $i++) {
+                $collaboration = new Collaboration;
+                $collaboration->setjoinedAt($generator->datetime('now', 'Europe/Paris'));
+                $collaboration->setUser($users[$i]);
+                $collaboration->setProject($project);
+                $manager->persist($collaboration);
+            }
         }
 
         $manager->flush();
